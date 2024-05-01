a89973791c00657702a99f31860d4c8fb915c15e
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-05-02 00:02:13 +0200

    moved secret_key to env file

--- parrot_hub/settings.py
+++ parrot_hub/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^z$8^$u2vn2%(vkxqtk(*3m2*=%68=sk*bts#%(qgs-a7-x)bs'
+# SECRET_KEY = 'django-insecure-^z$8^$u2vn2%(vkxqtk(*3m2*=%68=sk*bts#%(qgs-a7-x)bs'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

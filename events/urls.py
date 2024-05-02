92f1695629267ea3ca9b83e1cf39f1ac37ca6863
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-05-03 01:09:24 +0200

    Add events form for admin to add events

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
+from .views import add_event
 
 urlpatterns = [ 
   path("", views.event_list, name="events"),
+  path('add/', add_event, name='add_event'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.admin.views.decorators import staff_member_required
 from .models import Event
+from .forms import EventForm
 
 
 def event_list(request):
@@ -12,3 +14,20 @@ def event_list(request):
       "events": events
     }
   )
+
+@staff_member_required
+def add_event(request):
+    if request.method == 'POST':
+        event_form = EventForm(request.POST)
+        if event_form.is_valid():
+            event_form.save()
+            return redirect('events')  
+    else:
+        event_form = EventForm()
+    return render(
+        request, 
+        'events/add_event.html', 
+        {
+            'event_form': event_form
+        }
+      )

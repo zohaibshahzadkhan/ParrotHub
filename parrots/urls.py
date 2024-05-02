1fcc3fc5b28e7440f45985cbe55c1f7321c48261
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-05-02 18:38:18 +0200

    Add template for parrot detail

--- parrots/urls.py
+++ parrots/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [ 
-  path("", views.parrot_list, name="home")
+  path("", views.parrot_list, name="home"),
+  path("<slug:slug>/", views.parrot_detail, name="parrot_detail")
 ]
--- parrots/views.py
+++ parrots/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Parrot
 
 # Create your views here.
@@ -13,3 +13,15 @@ def parrot_list(request):
       "parrots": parrots
     }
   )
+
+def parrot_detail(request, slug):
+   queryset = Parrot.objects.filter(status=1)
+   parrot = get_object_or_404(queryset, slug=slug)
+
+   return render(
+      request,
+      "parrots/parrot_detail.html",
+      {
+        "parrot": parrot,         
+      },
+    )

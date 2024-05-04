9bbc7b61a0f06fc7684639945134c44aaefdd02c
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-05-04 23:44:12 +0200

    Add author field to Events modal

--- events/models.py
+++ events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -15,6 +16,7 @@ class Event(models.Model):
         location (str): The location of the event.
         status (int): The status of the event (0 for Draft, 1 for Published).
         created_on (DateTime): The date and time when the event was created.
+        author (User): The user who created the event :model:`auth.User`.
     """
 
     title = models.CharField(max_length=200)
@@ -24,6 +26,11 @@ class Event(models.Model):
     location = models.CharField(max_length=200)
     status = models.IntegerField(choices=STATUS, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name="event_created_by",
+        default=1, blank=True
+    )
 
     def __str__(self):
         """

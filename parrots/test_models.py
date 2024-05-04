b08bfcf594de85d57f193a9c24c3d0b22318ab24
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-05-04 09:37:39 +0200

    Add tests for parrots modal

---
+++ parrots/test_models.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from .models import Parrot, ParrotCategory
+
+class ParrotModelTestCase(TestCase):
+    def setUp(self):
+        self.category = ParrotCategory.objects.create(name='Test Category')
+
+        self.parrot = Parrot.objects.create(
+            name='Test Parrot',
+            slug='test-parrot',
+            characteristics='Test Characteristics',
+            image='test_image.jpg',
+            about='Test About',
+            category=self.category,
+            status=1
+        )
+
+    def test_parrot_model_fields(self):
+        # Test the fields of the Parrot model
+        self.assertEqual(self.parrot.name, 'Test Parrot')
+        self.assertEqual(self.parrot.slug, 'test-parrot')
+        self.assertEqual(self.parrot.characteristics, 'Test Characteristics')
+        self.assertEqual(self.parrot.image, 'test_image.jpg')
+        self.assertEqual(self.parrot.about, 'Test About')
+        self.assertEqual(self.parrot.category, self.category)
+        self.assertEqual(self.parrot.status, 1)
+
+    def test_parrot_str_representation(self):
+        # Test the string representation of the Parrot model
+        self.assertEqual(str(self.parrot), 'Test Parrot')
